6abdc1235c189ada2e35c8a5bc4b092cd5710fdd
Aiden Dickson <aidendickson@Aidens-MacBook-Pro.local>
2021-05-11 14:08:00 -0400

    adding html file knitted in order to test markdown

--- Shiny_Bar_App_Final/ui.R
+++ Shiny_Bar_App_Final/ui.R
@@ -10,7 +10,7 @@ all_ages <- read.csv("data/all-ages.csv")
 # Page 1 - First Tab [Introduction tab] --------------------
 
 first_tab <- tabPanel(
-  "Introduction",
+  "Introduction", 
   includeMarkdown("intro.Rmd")
 )
 
@@ -39,14 +39,14 @@ main_content <- mainPanel(width = "12",
     # Output: Tabset w/ plot, summary, and table (UI)----
     tabsetPanel(type = "tabs", 
                 tabPanel("Plot", plotOutput("plot", height='800')),
-                tabPanel("Summary", verbatimTextOutput('summary')),
-                tabPanel("Table", tableOutput("table"))
+                tabPanel("Table", tableOutput("table")),
+                tabPanel("Summary", verbatimTextOutput('summary'))
     )
 )
 
 #Second Panel, using top section input, below section plot
 second_tab <- tabPanel(
-  "Dataset",
+  "Dataset", 
    titlePanel("Economic Outcomes by College Major - Dataset"),
   p("Use the selector input below to choose which variable you would like to see."),
   
@@ -58,19 +58,14 @@ second_tab <- tabPanel(
 
 # Page 3 - Third Tab [testing] ---------------------------------------
 third_tab <- tabPanel( 
-  "Third",
+  "Analysis I: X", 
   
-  titlePanel("Test"),
+  titlePanel("Economic Outcomes by College Major - Analysis I: Blah of X"),
   
-  img(src = "college_dorm.png", height = 400, width = 700),
-  br(),  br(),
+  includeMarkdown("testing_file.html")
   
-  p("Analysis of Economic Outcomes by College Major."),  
-  
-  p(a(href = "https://www.kaggle.com/tunguz/college-majors?select=majors-list.csv", "Data Source [Kaggle]")),
- 
-  p(a(href = "https://github.com/aiden-dickson/DACSS-601/", "Project Repository on GitHub"))
-  
+
+
 )
 
 
